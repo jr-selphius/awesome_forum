ace752889e8d49596e8dbd9a783cc511b00cfa8f
jr-selphius <sanrafja@hotmail.com>
2019-05-15 18:12:57 +0200

    Added CommunityCreator, CommunitySearch tests

---
+++ src/test/jr/selphius/forum/module/community/CommunityUnitTestCase.scala
@@ -0,0 +1,18 @@
+package jr.selphius.forum.module.community
+
+import jr.selphius.forum.module.UnitTestCase
+import jr.selphius.forum.module.community.domain.{Community, CommunityRepository}
+
+protected[community] trait CommunityUnitTestCase extends UnitTestCase {
+  protected val repository: CommunityRepository = mock[CommunityRepository]
+
+  protected def shouldSearchAllCommunities(communities: Seq[Community]): Unit =
+    (repository.getAll _)
+      .expects()
+      .returning(communities)
+
+  protected def shouldSaveVideo(community: Community): Unit =
+    (repository.save _)
+      .expects(community)
+      .returning(())
+}
---
+++ src/test/jr/selphius/forum/module/community/application/CommunitySearcherTest.scala
@@ -0,0 +1,18 @@
+package jr.selphius.forum.module.community.application
+
+import jr.selphius.forum.module.community.CommunityUnitTestCase
+import jr.selphius.forum.module.community.domain.CommunityStub
+
+final class CommunitySearcherTest extends CommunityUnitTestCase {
+  private val communitySearcher = new CommunitiesSearcher(repository)
+
+  "Community Searcher" should {
+    "search all existing communities" in {
+      val existingCommunities = Seq(CommunityStub.random, CommunityStub.random)
+
+      shouldSearchAllCommunities(existingCommunities)
+
+      communitySearcher.searchAll() shouldBe existingCommunities
+    }
+  }
+}
